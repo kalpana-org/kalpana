092b1bd5e46a93c9862716bf989e5795620406e0
nycz <pyntix@gmail.com>
2013-01-20 12:26:24 +0100

    Renamed plugin.py -> pluginlib.py

--- pluginlib.py
+++ pluginlib.py
@@ -1,14 +1,11 @@
-NORTH = 0
-SOUTH = 1
-EAST = 2
-WEST = 3
+NORTH, SOUTH, EAST, WEST, = list(range(4))
 
 hotkeys = {}
 commands = {}
 
 class GUIPlugin:
 
-    def __init__(self, layout, get_text, add_widget):
+    def __init__(self, get_text, add_widget):
         self.get_text = get_text
 
     def read_config(self):
