8bf50ad61ce1c51475e249da4f5cd1dcc4ebd261
nycz <pyntix@gmail.com>
2013-02-08 14:46:04 +0100

    Updated copyright statment in linewidget.py

--- linewidget.py
+++ linewidget.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 John Schember
+# Copyright (C) 2009 John Schember, 2011-2013 nycz
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
