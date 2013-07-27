cd16f2df080cc8f1c20e2132737d464d9f131b2d
nycz <pyntix@gmail.com>
2013-07-27 21:59:32 +0200

    Rewrote config loading code and configfile format
    Now less old crappy code!! Also much more readable and unshitty stuff.
    Changed the format of defaultcfg.json as well.

--- kalpana.py
+++ kalpana.py
@@ -151,7 +151,7 @@ def connect_others_signals(mainwindow, textarea, terminal, settings_manager):
         (terminal.give_up_focus, textarea.setFocus),
         (terminal.goto_line, textarea.goto_line),
         (terminal.search_and_replace, textarea.search_and_replace),
-        (terminal.manage_settings, settings_manager.manage_settings),
+        (terminal.manage_settings, settings_manager.change_setting),
 
         # Settings manager
         (settings_manager.set_number_bar_visibility,
